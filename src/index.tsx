dc0187b8029e4e9b4c81f7950d6394433a6a1576
unknown <dang3rouz@ya.ru>
2020-10-21 16:07:23 +0500

    login via google

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Listings, Home, Host, Listing, NotFound, User } from "./sections";
+import { Layout } from "antd";
+import {
+  Listings,
+  Home,
+  Host,
+  Listing,
+  NotFound,
+  User,
+  Login,
+} from "./sections";
+import { Viewer } from "./lib/types";
 import * as serviceWorker from "./serviceWorker";
 import AppoloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
@@ -11,17 +21,34 @@ const client = new AppoloClient({
   uri: "/api",
 });
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
+
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  console.log(viewer);
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Host} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/user/:id" component={User} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login {...props} setViewer={setViewer} />}
+          />
+          <Route exact path="/user/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
